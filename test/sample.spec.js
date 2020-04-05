8a4f43e383be1eac8cdbc652882cc84f16b7e419
yuan liu <liuyzh1997@gmail.com>
2020-04-05 21:08:33 +0800

    first commit

--- test/sample.spec.js
+++ test/sample.spec.js
@@ -11,7 +11,7 @@ describe('add todo', function () {
     });
 
     it('should have correct title', async function() {
-        expect(await page.title()).to.eql('Puppeteer Demo');
+        expect(await page.title()).to.eql('Koa • Todo');
     })
     it('should new todo correct', async function() {
       await page.click('#new-todo', {delay: 500});
@@ -21,5 +21,53 @@ describe('add todo', function () {
       const expectInputContent = await page.evaluate(todoList => todoList.lastChild.querySelector('label').textContent, todoList);
       expect(expectInputContent).to.eql('new todo item');
     }) 
+
+  });
+
+  describe('todo-list', function () {
+    let page;
+
+    before (async function () {
+      page = await browser.newPage();
+      await page.goto('http://127.0.0.1:7001/');
+    });
+  
+    after (async function () {
+      await page.close();
+    });
+
+    it('should have correct title', async function() {
+        expect(await page.title()).to.eql('Koa • Todo');
+    })
+    it('should have correct todo list', async function() {
+      let todoList = await page.waitFor('#todo-list');
+      let todoLength = todoList.length;
+
+      expect(getByText(/public/javascripts/components/todo_list.js).todos.length).to.eql(todolength);
+    }) 
+
+  });
+
+  describe('completed', function () {
+    let page;
+
+    before (async function () {
+      page = await browser.newPage();
+      await page.goto('http://127.0.0.1:7001/');
+    });
+  
+    after (async function () {
+      await page.close();
+    });
+
+    it('should have correct title', async function() {
+        expect(await page.title()).to.eql('Koa • Todo');
+    })
+    it('should mark completed todo', async function() {
+      await page.click('.toggle', {delay: 500});
+      let todoList = await page.waitFor('#todo-list');
+      const classtype = await page.evaluate(todoList => todoList.lastChild.querySelector('li').class);
+      expect(classtype).to.eql('.completed');
+    })
 
   });
